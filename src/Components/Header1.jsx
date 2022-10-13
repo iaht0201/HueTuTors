018877be1f50254aef16bb4c86791a0262b0405f
iaht <klrt0120@gmail.com>
2022-10-14 00:59:31 +0700

    fixbug-navigater

--- src/Components/Header1.jsx
+++ src/Components/Header1.jsx
@@ -29,9 +29,9 @@ export default function Header1() {
     logOutAccount();
   };
   useEffect(() => {
-    if (supabase.auth.user() != null) {
-      navigate("/");
-    }
+    // if (supabase.auth.user() != null) {
+    //   navigate("/");
+    // }
   }, []);
 
   const navbar = [
